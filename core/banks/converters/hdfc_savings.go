1d33692c8e427059502c2ecf303d5ed89eb375f3
Shivansh Kuchchal <shivanshbox@gmail.com>
2022-10-20 20:11:02 +0530

    feat(core): icici-credit and hdfc-savings support added

--- core/banks/converters/hdfc_savings.go
+++ core/banks/converters/hdfc_savings.go
@@ -1,6 +1,9 @@
 package converters
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/shivanshkc/ledgerconv/core/models"
 )
 
@@ -16,5 +19,52 @@ func HDFCSavings(csvContent [][]string) ([]*models.ConvertedTransactionDoc, erro
 
 // hdfcSavingsRow is the rowConverterFunc for HDFC savings account statements.
 func hdfcSavingsRow(row []string) (*models.ConvertedTransactionDoc, bool, error) {
-	return nil, false, nil
+	// Due to some reason, the statements contain empty rows in between too.
+	if row[0] == "" {
+		// Skipping to the next row.
+		return nil, false, nil
+	}
+
+	// Parse timestamp.
+	timestamp, err := time.Parse("02/01/06", row[0])
+	if err != nil {
+		// If we fail to parse the timestamp, we consider it as the end of the transaction table.
+		return nil, true, nil //nolint:nilerr
+	}
+
+	// Other required fields.
+	remarks, serial := row[1], row[5]
+
+	// Get the amount information.
+	debitAmount, errDebit := strconv.ParseFloat(row[3], 64)
+	creditAmount, errCredit := strconv.ParseFloat(row[4], 64)
+
+	// If both amounts failed to parse, we cannot proceed further.
+	if errDebit != nil && errCredit != nil {
+		// If we fail to parse the amounts, we consider it as the end of the transaction table.
+		return nil, true, nil //nolint:nilerr
+	}
+
+	// Debit amount is taken negative.
+	amount := -debitAmount
+	// If debit amount is invalid or zero, we take the credit amount as the final value.
+	if errDebit != nil || debitAmount == 0 {
+		amount = creditAmount
+	}
+
+	// If the amount is zero, we do not consider this row.
+	if amount == 0 {
+		// Skipping to the next row.
+		return nil, false, nil
+	}
+
+	// Instantiating the transaction doc.
+	return &models.ConvertedTransactionDoc{
+		AccountName:     "", // This is not the responsibility of the converterFunc.
+		Amount:          amount,
+		Timestamp:       timestamp,
+		BankSerial:      serial,
+		BankPaymentMode: "",
+		BankRemarks:     remarks,
+	}, false, nil
 }
--- core/banks/converters/icici_credit.go
+++ core/banks/converters/icici_credit.go
@@ -1,6 +1,9 @@
 package converters
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/shivanshkc/ledgerconv/core/models"
 )
 
@@ -17,5 +20,47 @@ func ICICICredit(csvContent [][]string) ([]*models.ConvertedTransactionDoc, erro
 
 // iciciCreditRow is the rowConverterFunc for ICICI credit card account statements.
 func iciciCreditRow(row []string) (*models.ConvertedTransactionDoc, bool, error) {
-	return nil, false, nil
+	// Due to some reason, the statements contain empty rows in between too.
+	if row[0] == "" {
+		// Skipping to the next row.
+		return nil, false, nil
+	}
+
+	// Parse timestamp.
+	timestamp, err := time.Parse("02/01/2006", row[0])
+	if err != nil {
+		// If we fail to parse the timestamp, we consider it as the end of the transaction table.
+		return nil, true, nil //nolint:nilerr
+	}
+
+	// Other required fields.
+	serial, remarks, amountSign := row[1], row[2], row[6]
+
+	// Get the amount information.
+	amount, err := strconv.ParseFloat(row[5], 64)
+	if err != nil {
+		// If we fail to parse the amount, we consider it as the end of the transaction table.
+		return nil, true, nil //nolint:nilerr
+	}
+
+	// If the amount is zero, we do not consider this row.
+	if amount == 0 {
+		// Skipping to the next row.
+		return nil, false, nil
+	}
+
+	// Apparently, debit transactions have "CR" BillingAmountSign -_-
+	if amountSign == "CR" {
+		amount *= -1
+	}
+
+	// Instantiating the transaction doc.
+	return &models.ConvertedTransactionDoc{
+		AccountName:     "", // This is not the responsibility of the converterFunc.
+		Amount:          amount,
+		Timestamp:       timestamp,
+		BankSerial:      serial,
+		BankPaymentMode: "",
+		BankRemarks:     remarks,
+	}, false, nil
 }
